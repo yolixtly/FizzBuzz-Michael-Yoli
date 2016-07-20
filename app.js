c4fe1b9fb47d34623691e8d675aa8c7b63c7f50e
yolixtly <yolixtly_13@hotmail.com>
2016-07-20 12:03:16 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+/*
+
+a. function that takes care of the brains ( if a num 3 or 5 then log fizz or buzz. 
+b. Function that holds the input user 
+	1. Set a prompt/input to ask a user a number
+c. function to validate inputs  
+	2. Validation : 
+	- '3' transform strings into integer by adding a + sing parseInt()
+	- '3.3' Decimals 
+	-  'hello' parseInt() // logs NaN 
+d. function that runs the Game completly 
+e. function that sends a message to the user : your fizz is from 1 to inputNumber 
+f. function that handles the click event if we put a input 
+
+*/
+
